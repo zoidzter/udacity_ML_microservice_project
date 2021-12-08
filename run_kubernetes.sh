8a5de31b7159f9512abe6cc63c4c63a812b64a56
zoidzter <ec2-user@ip-172-31-13-194.ec2.internal>
2021-12-08 07:34:05 +0000

    Project submission

---
+++ run_kubernetes.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=zoidzter/api
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run api --image=$dockerpath --port=80
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward api 8000:80
